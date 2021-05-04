1c1e6cfab82b8a6a0a6c244d47516498a6e18ecf
shuklabhawna <83492227+shuklabhawna@users.noreply.github.com>
2021-05-04 20:55:09 +0800

    Create ExpenseDetailsRepositoryCustom.java

---
+++ src/main/java/com/example/sample/expenseTracker/repository/ExpenseDetailsRepositoryCustom.java
@@ -0,0 +1,14 @@
+
+package com.example.sample.expenseTracker.repository;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.example.sample.expenseTracker.model.ExpenseDetails;
+
+public interface ExpenseDetailsRepositoryCustom {
+	List<ExpenseDetails> findByParameter(Map<String, Object> queryParameter);
+	ExpenseDetails findOneByParameter(Map<String, Object> queryParameter);
+}
