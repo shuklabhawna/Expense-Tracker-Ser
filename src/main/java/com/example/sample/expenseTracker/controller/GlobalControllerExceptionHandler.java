273c2f634fa59eeb1c4f38be6b6556e135fc6e5c
shuklabhawna <83492227+shuklabhawna@users.noreply.github.com>
2021-05-04 20:50:08 +0800

    Create GlobalControllerExceptionHandler.java

---
+++ src/main/java/com/example/sample/expenseTracker/controller/GlobalControllerExceptionHandler.java
@@ -0,0 +1,207 @@
+
+package com.example.sample.expenseTracker.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.xml.bind.ValidationException;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.sample.expenseTracker.exception.ApplicationException;abstract
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+	private final Logger logger = LoggerFactory
+            .getLogger(GlobalControllerExceptionHandler.class);
+
+	 @ExceptionHandler(UnauthorizedException.class)
+	    public ResponseEntity<ErrorResponse> handleUnauthorizedException(
+	            UnauthorizedException ex) {
+	        logger.info("Unauthorized exception", ex);
+
+	        ErrorResponse errorResponse = new ErrorResponse();
+	        errorResponse.setErrorMessage("You are not authorized to perform this action. Please contact the system administrator.");
+	        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+	    }
+
+	    @ExceptionHandler(ConstraintViolationException.class)
+	    public ResponseEntity<ErrorResponse> handleMethodConstraintViolationException(
+	            ConstraintViolationException ex) {
+	        logger.info("Constraint violation exception", ex);
+
+	        Map<String, Object> errorMap = new HashMap<>();
+	        for (ConstraintViolation<?> constraintViolation : ex
+	                .getConstraintViolations()) {
+	            errorMap.put(constraintViolation.getPropertyPath().toString(),
+	                    constraintViolation.getMessage());
+	        }
+
+	        ErrorResponse errorResponse = new ErrorResponse();
+	        errorResponse.setErrorFields(errorMap);
+	        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	    }
+
+	    @ExceptionHandler(MethodArgumentNotValidException.class)
+	    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+	            MethodArgumentNotValidException ex) {
+	        logger.info("Method argument not valid exception", ex);
+
+	        Map<String, Object> errorMap = new HashMap<>();
+	        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+	            if (fieldError.getRejectedValue() == null) {
+	                errorMap.put(fieldError.getField(),
+	                        fieldError.getDefaultMessage());
+	            } else {
+	                errorMap.put(
+	                        fieldError.getField(),
+	                        fieldError.getDefaultMessage() + " ["
+	                                + fieldError.getRejectedValue() + "]");
+	            }
+	        }
+
+	        ErrorResponse errorResponse = new ErrorResponse();
+	        errorResponse.setErrorFields(errorMap);
+	        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	    }
+
+	    @ExceptionHandler(ValidationException.class)
+	    public ResponseEntity<ErrorResponse> handleValidationException(
+	            ValidationException ex) {
+	        logger.info("Validation exception", ex);
+
+	        ErrorResponse errorResponse = new ErrorResponse();
+	        errorResponse.setErrorMessage(ex.getMessage());
+
+	        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	    }
+
+	    @ExceptionHandler(DataIntegrityViolationException.class)
+	    public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(
+	            DataIntegrityViolationException ex) {
+	        logger.info("DataIntegrityViolationException exception", ex);
+
+	        ErrorResponse errorResponse = new ErrorResponse();
+	        errorResponse.setErrorMessage("Cannot save/update duplicate record.");
+
+	        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	    }
+
+	    @ExceptionHandler(OptimisticLockingFailureException.class)
+	    public ResponseEntity<ErrorResponse> handleOptimisticLockingFailureException(
+	            OptimisticLockingFailureException ex) {
+	        logger.info("OptimisticLockingFailureException exception", ex);
+
+	        ErrorResponse errorResponse = new ErrorResponse();
+	        errorResponse.setErrorMessage("Optimistic locking failed.");
+
+	        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	    }
+
+	    @ExceptionHandler(HttpMessageNotReadableException.class)
+	    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(
+	            HttpMessageNotReadableException ex) {
+	        logger.info("HttpMessageNotReadableException exception", ex);
+
+	        ErrorResponse errorResponse = new ErrorResponse();
+	        errorResponse
+	                .setErrorMessage("Unrecognized or incorrect message content.");
+
+	        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	    }
+
+	    @ExceptionHandler(MissingServletRequestParameterException.class)
+	    public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(
+	            MissingServletRequestParameterException ex) {
+	        logger.info("MissingServletRequestParameterException exception", ex);
+
+	        ErrorResponse errorResponse = new ErrorResponse();
+	        errorResponse.setErrorMessage(ex.getMessage());
+
+	        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	    }
+
+	    @ExceptionHandler(InvalidPasswordException.class)
+	    public ResponseEntity<ErrorResponse> handleAGCBaseException(
+	    		InvalidPasswordException ex) {
+
+	        logger.warn("InvalidPasswordException Support Code: [{}]",
+	                ex.getSupportCode());
+	        logger.warn("InvalidPasswordException", ex);
+
+	        ErrorResponse errorResponse = new ErrorResponse();
+	        errorResponse.setErrorMessage(ex.getSupportMessage());
+	        errorResponse.setErrorCode(ex.getSupportCode());
+	        return new ResponseEntity<>(errorResponse,
+	                HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	    @ExceptionHandler(InvalidUserAccountException.class)
+	    public ResponseEntity<ErrorResponse> handleAGCBaseException(
+	    		InvalidUserAccountException ex) {
+
+	        logger.warn("InvalidUserAccountException Support Code: [{}]",
+	                ex.getSupportCode());
+	        logger.warn("InvalidUserAccountException", ex);
+
+	        ErrorResponse errorResponse = new ErrorResponse();
+	        errorResponse.setErrorMessage(ex.getSupportMessage());
+	        errorResponse.setErrorCode(ex.getSupportCode());
+	        return new ResponseEntity<>(errorResponse,
+	                HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	    
+	    @ExceptionHandler(ApplicationException.class)
+	    public ResponseEntity<ErrorResponse> handleAGCBaseException(
+	            ApplicationException ex) {
+
+	        logger.error("ApplicationException Support Code: [{}]",
+	                ex.getSupportCode());
+	        logger.error("ApplicationException", ex);
+
+	        ErrorResponse errorResponse = new ErrorResponse();
+	        errorResponse.setErrorMessage(ex.getSupportMessage());
+	        errorResponse.setErrorCode(ex.getSupportCode());
+	        return new ResponseEntity<>(errorResponse,
+	                HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+
+	    @ExceptionHandler(RuntimeException.class)
+	    public ResponseEntity<ErrorResponse> handleRuntimeException(
+	            RuntimeException ex) {
+	        UUID errorCode = UUID.randomUUID();
+	        logger.error("RuntimeException Error Code: [{}]", errorCode);
+	        logger.error("RuntimeException", ex);
+
+	        ErrorResponse errorResponse = new ErrorResponse();
+	        errorResponse.setErrorMessage("System error occurred.");
+	        errorResponse.setErrorCode(errorCode.toString());
+	        return new ResponseEntity<>(errorResponse,
+	                HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+
+	    @ExceptionHandler(Exception.class)
+	    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+	        UUID errorCode = UUID.randomUUID();
+	        logger.error("Exception Error Code: [{}]", errorCode);
+	        logger.error("Exception", ex);
+
+	        ErrorResponse errorResponse = new ErrorResponse();
+	        errorResponse.setErrorMessage("System error occurred.");
+	        errorResponse.setErrorCode(errorCode.toString());
+	        return new ResponseEntity<>(errorResponse,
+	                HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+}
