50c968de88854c791ea5086b904c20382bc6af29
shuklabhawna <83492227+shuklabhawna@users.noreply.github.com>
2021-05-04 20:49:37 +0800

    Create AccountsRepository.java

---
+++ src/main/java/com/example/sample/expenseTracker/repository/AccountsRepository.java
@@ -0,0 +1,12 @@
+
+package com.example.sample.expenseTracker.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.sample.expenseTracker.model.Accounts;
+import com.example.sample.expenseTracker.model.User;
+
+public interface AccountsRepository extends JpaRepository<Accounts, Long>{
+
+	Accounts findByUser(User user);
+}
