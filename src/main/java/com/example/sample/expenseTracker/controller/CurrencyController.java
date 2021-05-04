e5d4e5dd648bea9a96dfb538108b0823f94e99b7
shuklabhawna <83492227+shuklabhawna@users.noreply.github.com>
2021-05-04 20:49:57 +0800

    Create CurrencyController.java

---
+++ src/main/java/com/example/sample/expenseTracker/controller/CurrencyController.java
@@ -0,0 +1,26 @@
+
+package com.example.sample.expenseTracker.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.sample.expenseTracker.model.Currency;
+import com.example.sample.expenseTracker.repository.CurrencyRepository;
+
+@RestController
+@RequestMapping("/currencies")
+public class CurrencyController {
+
+	
+	@Autowired
+	private CurrencyRepository currencyRepository;
+
+	@GetMapping("/list")
+	List<Currency> getCurrencies() {
+		return currencyRepository.findAll();
+	}
+}
