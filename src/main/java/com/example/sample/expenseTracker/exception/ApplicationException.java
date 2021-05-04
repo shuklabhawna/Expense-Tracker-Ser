93fbc425f6b9ee48de44d8fc967536e6adcf462e
shuklabhawna <83492227+shuklabhawna@users.noreply.github.com>
2021-05-04 20:53:53 +0800

    Create ApplicationException.java

---
+++ src/main/java/com/example/sample/expenseTracker/exception/ApplicationException.java
@@ -0,0 +1,17 @@
+
+package com.example.sample.expenseTracker.exception;
+
+public class ApplicationException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+
+	    public ApplicationException(String message) {
+	        super(message);
+	    }
+
+	    public ApplicationException(String message, Throwable cause) {
+	        super(message, cause);
+	    }
+
+}
